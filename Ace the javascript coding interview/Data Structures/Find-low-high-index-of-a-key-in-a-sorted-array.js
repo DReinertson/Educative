5180e215485b651330742d3483b3400f73425f45
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-11-19 07:55:21 -0800

    Completed problem - binary search

--- Ace the javascript coding interview/Data Structures/Find-low-high-index-of-a-key-in-a-sorted-array.js
+++ Ace the javascript coding interview/Data Structures/Find-low-high-index-of-a-key-in-a-sorted-array.js
@@ -1,15 +1,54 @@
 let findLowIndex = function(arr, key) {
   //TODO: Write - Your - Code
-  for (let x = 0; x < arr.length; x++){
-    if(arr[x] === key) return x;
+  //binary search method
+  let start = 0; 
+  let end = arr.length - 1;
+  let mid = Math.floor(end / 2);
+
+  while (start <= end){
+    
+
+    if (arr[mid] < key){
+      start = mid + 1;
+    } else{
+      end = mid - 1;
+    }
+
+    mid = start + Math.floor((end - start) / 2);
   }
+
+  
+    if (start < arr.length && arr[start] === key){
+      return start;
+    }
+
+  
   return -1;
 };
 
 let findHighIndex = function(arr, key) {
   //TODO: Write - Your - Code
-  for (let x = arr.length-1; x >= 0; x--){
-    if (arr[x] === key) return x;
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor(end / 2);
+
+  while (start <= end){
+
+    if (arr[mid] <= key){
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+    
+    mid = start + Math.floor((end - start) / 2);
+    
   }
+
+  if (end === -1){
+    return end;
+  }  
+    if (end < arr.length && arr[end] === key){
+      return end;
+    }
   return -1;
 };
