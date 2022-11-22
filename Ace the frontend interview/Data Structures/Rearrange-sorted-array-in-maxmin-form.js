442fc1b3ec94a9925f437650bbe6a46bebbffed8
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-11-22 00:51:04 -0800

    Completed problem - without break

--- Ace the frontend interview/Data Structures/Rearrange-sorted-array-in-maxmin-form.js
+++ Ace the frontend interview/Data Structures/Rearrange-sorted-array-in-maxmin-form.js
@@ -3,17 +3,14 @@ function maxMin(arr){
   let right = arr.length - 1;
   let newArr = [];
 
-  while (left <= right){
-    if (left === right){
-      newArr.push(arr[left]);
-      break;
-    } else{
+  while (left < right){
       newArr.push(arr[right]);
       newArr.push(arr[left]);
       right--;
       left++;
-    }
+  }
 
-  } 
+  newArr.push(arr[left]);
+
   return newArr;
 }
