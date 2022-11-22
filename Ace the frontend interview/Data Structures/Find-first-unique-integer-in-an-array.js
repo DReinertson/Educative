faa84d19c555b9045f3a9093d238a6a81663b779
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-11-22 00:10:47 -0800

    Completed problem - nested loops

--- Ace the frontend interview/Data Structures/Find-first-unique-integer-in-an-array.js
+++ Ace the frontend interview/Data Structures/Find-first-unique-integer-in-an-array.js
@@ -1,18 +1,16 @@
 function findFirstUnique(arr) {
-   let map1 = new Map();
+   
 
    for (let x = 0; x < arr.length; x++){
-      if (!map1.has(arr[x])){
-         map1.set(arr[x], 1);
-      } else{
-         map1.set(arr[x], map1.get(arr[x])+1);
+      let unique = true;
+      for (let y = x+1; y < arr.length; y++){
+         if (arr[x] === arr[y]){
+            unique = false;
+            break;
+         }
       }
-   }
-
-   for (let y = 0; y < arr.length; y++){
-      if (map1.get(arr[y]) === 1){
-         return arr[y];
-      }
+      if (unique === true)
+      return arr[x];
    }
    return null;;
 }
